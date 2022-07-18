bf587f45681815c7b8a55392d64b2be35970d20a
unforgivab1e <285182272@qq.com>
2022-07-18 14:16:37 +0800

    init

---
+++ src/main/java/com/example/blogs/controllers/BlogController.java
@@ -0,0 +1,12 @@
+package com.example.blogs.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blog")
+public class BlogController {
+
+
+}
